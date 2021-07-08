b498198b5bc801adffde19e30ace2c41a94942bc
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-07-08 10:13:44 -0300

    Add files via upload

---
+++ funcion_5.c
@@ -0,0 +1,21 @@
+/*
+	https://elcodigoascii.com.ar/
+
+	Conociendo que al tipo de dato char tambien lo podemos  usar 
+	con sus codigos numeros de la tabla ascii, hagamos un programa 
+	para poder utilizar todos los caracteres del abecedario en minuscula
+
+
+	Requisitos
+		- mostrar un menu
+		- Imprimir el abecedario en minuscula
+			- imprimir todo el abecedario
+			- imprimir un rango especifico
+			- imprimir desde un caracter especifico
+		- repetir hasta que el usuario quiera salir
+		- manejar solo codigo ascii 
+	
+	EXTRA: permitir mostrar en mayuscula
+	EXTRA: permitir alternar entre minuscula y mayuscula en un mismo
+			resultado de impresion
+*/
