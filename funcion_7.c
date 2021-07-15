7f0649b27aaac2422b4e2b4461112b8eea3337e5
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-07-15 12:10:47 -0300

    Add files via upload

---
+++ funcion_7.c
@@ -0,0 +1,34 @@
+/*
+	Devolver cambio
+
+	Vamos a configurar una maquina para poder devolver el cambio
+	correcto
+
+	Aspectos a tener en cuenta:
+		- Hay 3 tipos de monedas a usar
+			- de 75 centavos
+			- de 25 centavos
+			- de 1 centavos
+		- La unidad en todo el programa es en centavos
+
+	Requisitos:
+		- Generar un cambio aleatorio con la funcion obtenerCambio
+		- Devolver la cantidad de monedas necesarias para el cambio total
+		- Informar lo necesario para el usuario
+
+		Ej Con un numero aleatorio entre 10 y 20 entonces:
+			Ejecucion 1:
+				Cambio: 803
+					Monedas de 75: 10
+					Monedas de 25: 2
+					Monedas de 1: 3
+			Ejecucion 1:
+				Cambio: 1168
+					Monedas de 75: 15
+					Monedas de 25: 1
+					Monedas de 1: 18
+
+
+	Extra 1: Implementar el mismo ejercicio desde 0 con modularizacion
+	Extra 2: Pedir el ingreso de las monedas a usar en todo el programa
+*/
